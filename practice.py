d3a3745c7e768554ddacc2b27ec81cfc818c734a
surajdongaree <115631669+surajdongaree@users.noreply.github.com>
2022-10-16 18:50:19 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -12,52 +12,66 @@ driver.get("https://demoblaze.com/index.html")
 
 driver.maximize_window()
 
-login = driver.find_element(By.ID, 'login2').click()
-
+login = driver.find_element(By.ID, 'login2')
+login.click()
 driver.implicitly_wait(10)
-# driver.find_element(By.XPATH, './/*[@id="logInModal"]/div/div').click()
-username = driver.find_element(By.ID, 'loginusername').send_keys('sctqaautomation@grr.la')
-time.sleep(5)
-password = driver.find_element(By.ID, 'loginpassword').send_keys("Spring@123")
 
-button = driver.find_element(By.XPATH, "//button[normalize-space()='Log in']").click()
+username = driver.find_element(By.ID, 'loginusername')
+username.send_keys('sctqaautomation@grr.la')
+time.sleep(5)
+password = driver.find_element(By.ID, 'loginpassword')
+password.send_keys("Spring@123")
+
+button = driver.find_element(By.XPATH, "//button[normalize-space()='Log in']")
+button.click()
 
 time.sleep(10)
-phone = driver.find_element(By.XPATH, "//div[@class='list-group']/a[2]").click()
+phone = driver.find_element(By.XPATH, "//div[@class='list-group']/a[2]")
+phone.click()
 
 # add samsung phone
-samsungPhone = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[1]/div/div/h4/a").click()
-addcart = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a").click()
+samsungPhone = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[1]/div/div/h4/a")
+samsungPhone.click()
+addCart = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a")
+addCart.click()
 time.sleep(10)
 Alert(driver).accept()
 
 # add laptop
-home_menu = driver.find_element(By.XPATH, "//*[@id='navbarExample']/ul/li[1]/a").click()
-laptops = driver.find_element(By.XPATH, "//div[@class='list-group']/a[3]").click()
+home_menu = driver.find_element(By.XPATH, "//*[@id='navbarExample']/ul/li[1]/a")
+home_menu.click()
+laptops = driver.find_element(By.XPATH, "//div[@class='list-group']/a[3]")
+laptops.click()
 time.sleep(5)
-laptop = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[4]/div/a").click()
-addcart2 = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a").click()
+laptop = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[4]/div/a")
+laptop.click()
+addCart2 = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a")
+addCart2.click()
 time.sleep(5)
 Alert(driver).accept()
 
 # add monitor
-home_menu2 = driver.find_element(By.XPATH, "//*[@id='navbarExample']/ul/li[1]/a").click()
+home_menu2 = driver.find_element(By.XPATH, "//*[@id='navbarExample']/ul/li[1]/a")
+home_menu2.click()
 time.sleep(5)
-monitors = driver.find_element(By.XPATH, "//div[@class='list-group']/a[4]").click()
-asusfullhd = driver.find_element(By.XPATH, "//a[normalize-space()='ASUS Full HD']").click()
-addcart3 = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a").click()
+monitors = driver.find_element(By.XPATH, "//div[@class='list-group']/a[4]")
+monitors.click()
+asusFullhd = driver.find_element(By.XPATH, "//a[normalize-space()='ASUS Full HD']")
+asusFullhd.click()
+addCart3 = driver.find_element(By.XPATH, "//*[@id='tbodyid']/div[2]/div/a")
+addCart3.click()
 time.sleep(5)
 Alert(driver).accept()
 
 # navigate to cart
-cart = driver.find_element(By.XPATH, "//*[@id='cartur']").click()
+cart = driver.find_element(By.XPATH, "//*[@id='cartur']")
+cart.click()
 
 # total order
 totalOrder = driver.find_elements(By.XPATH, "//*[@id='tbodyid']/tr")
-print(totalOrder)
 print('total order:', len(totalOrder))
 
-
+# sorted list
 text_name = [el.text for el in driver.find_elements(By.XPATH, "//*[@id='tbodyid']/tr/td[3]")]
 text_name1 = sorted(text_name)
 print("sorted list:", text_name1)
@@ -85,25 +99,35 @@ else:
 print("Sum of all elements in given list: ", total)
 
 # place order
-placeOrder = driver.find_element(By.XPATH, "//*[@id='page-wrapper']/div/div[2]/button").click()
+placeOrder = driver.find_element(By.XPATH, "//*[@id='page-wrapper']/div/div[2]/button")
+placeOrder.click()
 
-name = driver.find_element(By.ID, "name").send_keys("suraj")
-country = driver.find_element(By.ID, "country").send_keys("india")
-city = driver.find_element(By.ID, "city").send_keys("pune")
+name = driver.find_element(By.ID, "name")
+name.send_keys("suraj")
+country = driver.find_element(By.ID, "country")
+country.send_keys("india")
+city = driver.find_element(By.ID, "city")
+city.send_keys("pune")
 time.sleep(3)
-creditCart = driver.find_element(By.ID, "card").send_keys("123489")
-month = driver.find_element(By.ID, "month").send_keys("april")
-year = driver.find_element(By.ID, "year").send_keys("2022")
+creditCart = driver.find_element(By.ID, "card")
+creditCart.send_keys("123489")
+month = driver.find_element(By.ID, "month")
+month.send_keys("april")
+year = driver.find_element(By.ID, "year")
+year.send_keys("2022")
 time.sleep(5)
-purchase = driver.find_element(By.XPATH, "//*[@id='orderModal']/div/div/div[3]/button[2]").click()
+purchase = driver.find_element(By.XPATH, "//*[@id='orderModal']/div/div/div[3]/button[2]")
+purchase.click()
 time.sleep(5)
 
 ids = driver.find_element(By.XPATH, "/html/body/div[10]/p").text
-print("item information:", ids)
-
+idList = ids.split(":")[0:3]
+idJoin = ":".join(idList).strip("\nCard Number")
+print("id and amount:", idJoin)
 
 time.sleep(5)
-okbtn = driver.find_element(By.XPATH, "//button[normalize-space()='OK']").click()
+okBtn = driver.find_element(By.XPATH, "//button[normalize-space()='OK']")
+okBtn.click()
 
 driver.quit()
 
